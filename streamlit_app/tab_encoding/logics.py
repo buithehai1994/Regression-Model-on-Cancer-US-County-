2966b3253876dac3c6f4dea6d2ea1f7290fa8040
buithehai1994 <72820310+buithehai1994@users.noreply.github.com>
2024-02-13 21:39:37 +1100

    Update logics.py

--- streamlit_app/tab_encoding/logics.py
+++ streamlit_app/tab_encoding/logics.py
@@ -23,7 +23,18 @@ class Encoding:
         self.data['Geography'] = geography_encoder.fit_transform(self.data['Geography'])
         self.data['binnedInc'] = binnedInc_encoder.fit_transform(self.data['binnedInc'])
         return self.data
+        
+    def multivarate_process(self):
+        if self.data is None:
+            raise ValueError("No data available. Use set_data() to load data first.")
+        
 
+        self.data = self.data.copy()
+        self.data=self.data.drop('PctSomeCol18_24',axis=1)
+        self.data['PctEmployed16_Over'] = self.data['PctEmployed16_Over'].fillna(df['PctEmployed16_Over'].mean())
+        self.data['PctPrivateCoverageAlone'] = self.data['PctPrivateCoverageAlone'].fillna(df['PctPrivateCoverageAlone'].mean())
+        return self.data
+        
     @st.cache
     def head_df(self):
         if self.data is not None:
