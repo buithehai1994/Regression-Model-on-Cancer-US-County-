40895012c7a7b4f79b0bb683bfb626dd18290018
buithehai1994 <72820310+buithehai1994@users.noreply.github.com>
2024-02-14 00:31:33 +1100

    Update app.py

--- streamlit_app/app/app.py
+++ streamlit_app/app/app.py
@@ -162,7 +162,7 @@ elif selected_tab == "Machine Learning Model":
             st.write("    ")
             
             st.write("Testing chart")
-            display_chart(x_test,y_test,y_test_preds)
+            display_chart(X_test,y_test,y_test_preds)
 
             st.write("MSE of Testing: ", mse_test_score)
             st.write("MAE of Testing: ", mae_test_score)
@@ -259,7 +259,7 @@ elif selected_tab == "Machine Learning Model":
         mae_test_score = mae(y_test, y_test_preds)
         
         st.write("Training chart")
-        display_chart(x_train,y_train,y_train_preds)
+        display_chart(X_train,y_train,y_train_preds)
 
         st.write("MSE of Training: ", mse_train_score)
         st.write("MAE of Training: ", mae_train_score)
