13bef9f6cfec5012e5f6cbace17d036e714da238
buithehai1994 <72820310+buithehai1994@users.noreply.github.com>
2024-02-14 21:34:15 +1100

    Update app.py

--- streamlit_app/app/app.py
+++ streamlit_app/app/app.py
@@ -473,11 +473,10 @@ elif selected_tab == "Machine Learning Model":
         ) 
 
         st.markdown("<h1 style='font-size: 32px; font-weight: bold;margin-right: 100px;'>Testing set</h1>", unsafe_allow_html=True)
-        st.altair_chart(final_chart, use_container_width=True)
-
         st.write("MSE of Testing: ", mse_test_score)
-        st.write("MAE of Testing: ", mae_test_score)      
-
+        st.write("MAE of Testing: ", mae_test_score)   
+        st.altair_chart(final_chart, use_container_width=True)
+   
     if selected_sub_tab == tab_titles[2]:
         X =data_for_ml_feature_engineering.drop(['TARGET_deathRate','avgDeathsPerYear','avgAnnCount','popEst2015','povertyPercent','MedianAgeMale',
             'MedianAgeFemale','PctPrivateCoverage','PctPrivateCoverageAlone','PctUnemployed16_Over',
