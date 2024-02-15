a9a815e15604af17acf6bbc8b79248b23195fb8d
buithehai1994 <72820310+buithehai1994@users.noreply.github.com>
2024-02-15 20:32:12 +1100

    Update display.py

--- streamlit_app/tab_encoding/display.py
+++ streamlit_app/tab_encoding/display.py
@@ -2,14 +2,25 @@ import streamlit as st
 from tab_encoding.logics import Encoding
 from tab_eda.logics import EDA
 
-def display_tab_df_encoding_explain(dataset):
-    st.subheader("Encoded DataFrame:")
-    
+def display_code_explanation():
     explanation_text = """
-    """
+    # Code Explanation
     
+    **Using LabelEncoder for Categorical Encoding:**
+    
+    The provided code snippet utilizes LabelEncoder instances to encode categorical features, 'Geography' and 'binnedInc', into numeric labels.
+
+    **1. Encoding 'Geography':**
+    
+    The 'Geography' feature likely represents geographic regions or locations. By encoding it using a LabelEncoder, the categorical values are converted into numeric labels. This enables machine learning algorithms to process geographic information effectively.
+    
+    **2. Encoding 'binnedInc':**
+    
+    The 'binnedInc' feature likely represents income levels that have been binned or grouped into categories. Encoding it allows the algorithm to interpret income-related information numerically, facilitating computations and analysis involving income data.
+    
+    Encoding categorical features is a crucial preprocessing step in machine learning tasks, as it transforms non-numeric data into a format that algorithms can handle.
+    """
     st.markdown(explanation_text)
-    st.write(dataset.head())  # Display or return the encoded data frame
 
 def display_correlation_encoding_heatmap(dataset):
     eda = EDA(dataset)
