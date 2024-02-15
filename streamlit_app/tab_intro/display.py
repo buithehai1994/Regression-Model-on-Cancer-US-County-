d2eddc90ff61a34db07ef1b074bac127480fc599
buithehai1994 <72820310+buithehai1994@users.noreply.github.com>
2024-02-16 09:43:20 +1100

    Update display.py

--- streamlit_app/tab_intro/display.py
+++ streamlit_app/tab_intro/display.py
@@ -3,7 +3,7 @@ import streamlit as st
 def introduction():
     st.write("""
     During this project, I will perform linear regression models on Cancer Death Rate of US counties. 
-    The dataset has one independent variable TARGET deathrate and 34 independent variables. 
+    The dataset has one independent variable **TARGET deathrate** and 34 independent variables. 
     The project is divided into three main parts. 
     The first part is the performance of univariate linear regression on two different variables. 
     The second part will perform multivariate linear regression on various variables. 
